d195e97e5036d08c68b4eff5c31b765f8239a9a9
CaseyDragon <cndragon83@gmail.com>
2022-12-04 15:58:05 -0500

    start of frontend

--- burger-express/src/App.js
+++ burger-express/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import "bootswatch/dist/superhero/bootstrap.min.css";
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Welcome from './Components/Welcome.js';
+import BurgerView from './Components/BurgerView.js';
+import EachBurger from './Components/EachBurger.js';
+
+
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [burgers, setBurgers] = useState([])
+ 
+//api call
+  // const getBurgers = {
+  //   method: 'GET',
+  //   url: 'whatever the burger Url is',
+  //   headers: {
+  //     // 'X-RapidAPI-Key': '2d1bb0d53fmsh47470deb9c91173p143b5bjsn9620ab6bef18',
+  //     // 'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com',
+  //   }
+  // };
+
+  // axios.request(getBurgers).then(function (response) {
+  //   setBurgers(response.data);
+  //   setLoading(true);
+
+  // }).catch(function (error) {
+  //   console.error(error);
+  // })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="welcome">
+          <Welcome />
+        </div>
+        <Routes>
+          <Route path="burgerview" element={<BurgerView loading={loading}  />} />
+          <Route path="/" />
+          <Route path="/burgerview/:burger" element={<EachBurger burgers={burgers}  />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ burger-express/src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const Welcome = () => {
+    return (
+        <div>
+            <div className="nameBar">Welcome to Burger Express!</div>
+
+            <Link to='burgerView'>
+                <button className="pointer" >Burger Menu</button>
+            </Link>
+        </div>
+    )
+}
+
+export default Welcome;

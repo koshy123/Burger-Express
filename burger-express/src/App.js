aed16424a307652d8cf3a163aa45ec42e2564a31
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-05 16:25:08 -0500

    dont need these changes

--- burger-express/src/App.js
+++ burger-express/src/App.js
@@ -1,53 +1,37 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom/'
-// import "bootswatch/dist/superhero/bootstrap.min.css";
-import { useState } from 'react';
 import './App.css';
-import Welcome from './components/Welcome.js';
-import Burger from './components/BurgerView.js';
-import EachBurger from './components/EachBurger.js';
+
+
+// import { Routes, Route } from 'react-router-dom/'
+// // import "bootswatch/dist/superhero/bootstrap.min.css";
+// import { useState } from 'react';
+// import Welcome from './components/Welcome.js';
+// import Burger from './components/BurgerView.js';
+// import EachBurger from './components/EachBurger.js';
 
 
 
 function App() {
   // const [loading, setLoading] = useState(false)
-  const [burgers, setBurgers] = useState([])
+  // const [burgers, setBurgers] = useState([])
  
-//api call
-  // const getBurgers = {
-  //   method: 'GET',
-  //   url: 'whatever the burger Url is',
-  //   headers: {
-  //     // 'X-RapidAPI-Key': '2d1bb0d53fmsh47470deb9c91173p143b5bjsn9620ab6bef18',
-  //     // 'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com',
-  //   }
-  // };
-
-  // axios.request(getBurgers).then(function (response) {
-  //   setBurgers(response.data);
-  //   setLoading(true);
-
-  // }).catch(function (error) {
-  //   console.error(error);
-  // })
 
   return (
 
-    // <BrowserRouter>
       <div className="App">
         <div className="welcome">
-          <h1> are we working here?</h1>
-          <Welcome />
+          <h1> Burger Express</h1>
+          {/* <Welcome /> */}
         </div>
-        <Routes>
+        {/* <Routes>
           <Route path="burgerview" element={<Burger />} />
           <Route path="*" element ={<App/>}/>
           <Route path="/burgerview/:burger" element={<EachBurger burgers={burgers}  />} />
-        </Routes>
+        </Routes> */}
 
       </div>
-    // </BrowserRouter>
+     
 
 
   );
